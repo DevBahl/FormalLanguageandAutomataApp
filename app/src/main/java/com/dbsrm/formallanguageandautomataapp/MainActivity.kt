ca39ad95b4310d25e7858e980a00c596ff0059c6
Dev Bahl <devbahl12@yahoo.com>
2020-10-10 02:42:31 +0530

    final

--- app/src/main/java/com/dbsrm/formallanguageandautomataapp/MainActivity.kt
+++ app/src/main/java/com/dbsrm/formallanguageandautomataapp/MainActivity.kt
@@ -4,32 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), ChipNavigationBar.OnItemSelectedListener {
 
-    var bottomNavigationView: BottomNavigationView? = null
+    var bottomNavigationView: ChipNavigationBar? = null
     val fm = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomNavigationView?.itemIconTintList = null
-        bottomNavigationView?.setOnNavigationItemSelectedListener(this)
-        bottomNavigationView?.selectedItemId = R.id.home
+        bottomNavigationView!!.setOnItemSelectedListener(this)
+        bottomNavigationView!!.setItemSelected(R.id.home)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+    override fun onItemSelected(id: Int) {
+        when (id) {
             R.id.home -> {
                 fm.beginTransaction().replace(R.id.container, home()).commit()
-                return true
             }
             R.id.about -> {
                 fm.beginTransaction().replace(R.id.container, about()).commit()
-                return true
             }
         }
-        return false
     }
 }
